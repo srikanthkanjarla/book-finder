81520cbeb29e853d0267410351a5af753cbe7723
srikanthkanjarla <srikanthkanjarla@gmail.com>
2019-03-03 23:54:07 +0530

    Update book card content

--- src/Book.js
+++ src/Book.js
@@ -4,6 +4,7 @@ import { withStyles } from '@material-ui/core';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
 
 const styles = {
   card: {
@@ -12,7 +13,6 @@ const styles = {
     margin: '45px 15px',
     padding: 10,
     maxWidth: 355,
-    overflow: 'visible',
   },
   cover: {
     flex: 1,
@@ -20,43 +20,49 @@ const styles = {
     width: 128,
     height: 171,
     backgroundSize: 'auto',
-    position: 'relative',
-    top: '-40px',
   },
   content: {
     flex: 2,
   },
-  noBooksMessage: {
-    fontSize: '24px',
-    textAlign: 'center',
-  },
 };
 // img src,
-const BookItem = props => {
-  const { classes, thumbnail, title, authors, publisher } = props;
-  console.log(author);
+const Book = props => {
+  const { classes, thumbnail, title, authors, publisher, pageCount, previewLink } = props;
   const author = authors.map(person => person);
   return (
     <Card className={classes.card}>
       <CardMedia image={thumbnail} title="book cover" className={classes.cover} />
       <CardContent className={classes.content}>
-        <h4 style={{ textAlign: 'center' }}>{title}</h4>
+        <h4>{title}</h4>
         <p>
-          Author
-          {author}
+          <span>By - </span>
+          <span>{author}</span>
         </p>
         <p>
-          publisher
-          {publisher}
+          <span>Publisher - </span>
+          <span>{publisher}</span>
         </p>
+        <p>
+          <span>{pageCount}</span>
+          <span> pages</span>
+        </p>
+        <Button variant="contained" href={previewLink} target="_blank" className={classes.button} color="primary">
+          See this Book
+        </Button>
       </CardContent>
     </Card>
   );
 };
-// BookItem.propTypes = {
-//   classes: PropTypes.shape({
-//     // bookList: PropTypes.string.isRequired,
-//     noBooksMessage: PropTypes.string.isRequired,
-//   }).isRequired,
-// };
-export default withStyles(styles)(BookItem);
+Book.propTypes = {
+  classes: PropTypes.objectOf(PropTypes.string.isRequired).isRequired,
+  authors: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  publisher: PropTypes.string,
+  pageCount: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  previewLink: PropTypes.string.isRequired,
+  thumbnail: PropTypes.string.isRequired,
+};
+Book.defaultProps = {
+  publisher: '',
+};
+export default withStyles(styles)(Book);
