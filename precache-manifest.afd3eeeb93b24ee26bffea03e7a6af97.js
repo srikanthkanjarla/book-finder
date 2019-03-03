21d7dc2e8207d871e64408f523323aa345ba034e
srikanthkanjarla <srikanthkanjarla@gmail.com>
2019-03-04 01:15:44 +0530

    Updates

---
+++ precache-manifest.afd3eeeb93b24ee26bffea03e7a6af97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "736fd4229ce5946055cf",
+    "url": "/book-finder/static/js/runtime~main.736fd422.js"
+  },
+  {
+    "revision": "9c28569180340baedc32",
+    "url": "/book-finder/static/js/main.9c285691.chunk.js"
+  },
+  {
+    "revision": "ec319395f44a64295150",
+    "url": "/book-finder/static/js/2.ec319395.chunk.js"
+  },
+  {
+    "revision": "9c28569180340baedc32",
+    "url": "/book-finder/static/css/main.28c4009d.chunk.css"
+  },
+  {
+    "revision": "72c3c2eb22bec095be5aa22432347557",
+    "url": "/book-finder/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.46e7b72cb49dac62091ea240d1bf883a.js"
+  "/book-finder/precache-manifest.afd3eeeb93b24ee26bffea03e7a6af97.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/book-finder/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
